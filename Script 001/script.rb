cf526fa56eb83e29d91eddb1643cff8bfd21b137
Biwy <biwardounet@gmail.com>
2014-05-30 22:50:35 +0200

    Update script.rb
    Biwy : Ajout de la gestion des touches clavier (Echap & Entrée)

--- Script 001/script.rb
+++ Script 001/script.rb
@@ -23,31 +23,61 @@ class Scene_Menu
   # * Start
   #---------------------------------
   def start
-    super
-        super
-create_title  #Commande "Afficher le titre du jeu"
-end  
-def create_title  #On va définir la commande "Afficher le titre du jeu"
-@title = Window_Help.new(1) # Création de la fenetre pour afficher le titre du jeu
-@title.set_text($data_system.game_title) # @title affiche le nom du jeu alias $data_system.game_title
-
-end
-  end
+    super # Héritage du start de Scene_Base
+    create_title  # Commande "Afficher le titre du jeu"
+  end  
   
+  #---------------------------------
+  # * Create Title
+  # = Affichage du titre
+  #---------------------------------
+  def create_title  #On va définir la commande "Afficher le titre du jeu"
+    @title = Window_Help.new(1) # Création de la fenêtre pour afficher le titre du jeu
+    @title.set_text($data_system.game_title) # @title affiche le nom du jeu alias $data_system.game_title
+  end
+
   #---------------------------------
   # * Terminate
   #---------------------------------
   def terminate
-    super
-    
+    super # Héritage du terminate de Scene_Base
+    @title.dispose # On enlève la fenêtre qui affiche le titre du jeu
   end    
 
   #---------------------------------
   # * Update
   #---------------------------------
   def update
-    super
-    
+    super # Héritage du update de Scene_Base
+    update_input # Mise à jour des appuis de touches
+  end
+  
+  #---------------------------------
+  # * Update Input
+  # = Mise à jour des appuis de touches
+  #---------------------------------
+  def update_input
+    if Input.trigger?(:B) # Si la touche B (= Echap ou X) est pressée...
+      update_input_b # ...alors on va réaliser ce qui se trouve dans la méthode update_input_b
+    elsif Input.trigger?(:C) # Si la touche C (= Entrée) est pressée...
+      update_input_c # ...alors on va réaliser ce qui se trouve dans la méthode update_input_c
+    end
+  end
+  
+  #---------------------------------
+  # * Update Input B
+  # = Methode qui se réalise si la touche B (= Echap ou X) est pressée
+  #---------------------------------
+  def update_input_b
+    # Mettre ici ce qu'il se passe quand on appuie sur B (= Echap ou X)
+  end
+  
+  #---------------------------------
+  # * Update Input C
+  # = Méthode qui se réalise si la touche C (= Entrée) est pressée
+  #---------------------------------
+  def update_input_c
+    # Mettre ici ce qu'il se passe quand on appuie sur C (= Entrée)
   end
 end
 #######################################################################

e06cf8d425c39def06a0ced0df266562462a8cb5
Arxwel <romainpasdeloup@outlook.com>
2014-05-30 22:31:52 +0200

    Update script.rb
    Modification de Arx/Guiop 12:
    Commande afficher titre du jeu

--- Script 001/script.rb
+++ Script 001/script.rb
@@ -6,8 +6,8 @@
 #### Participants  :
 # Bibi : 
 # - architecture du nouvel objet Scene_Menu
-#
-#
+# Arx/Guiop12 :
+# - Afficher Titre du jeu
 #
 #
 #######################################################################
@@ -24,7 +24,14 @@ class Scene_Menu
   #---------------------------------
   def start
     super
-    
+        super
+create_title  #Commande "Afficher le titre du jeu"
+end  
+def create_title  #On va définir la commande "Afficher le titre du jeu"
+@title = Window_Help.new(1) # Création de la fenetre pour afficher le titre du jeu
+@title.set_text($data_system.game_title) # @title affiche le nom du jeu alias $data_system.game_title
+
+end
   end
   
   #---------------------------------
